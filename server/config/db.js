6cfc4b71cc4e13f4c49d276023c65e99bbc21032
Robin Bararu <bararu7@hotmail.com>
2021-10-29 08:56:39 -0400

    updated URI for mongoDB

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/bookStore"
+  //"URI": "mongodb://localhost/bookStore"
+  "URI" : "mongodb+srv://robin:X0DzsqQekAlnITt2@mongodbserver.0y2mv.mongodb.net/bookStore?retryWrites=true&w=majority"
 };
